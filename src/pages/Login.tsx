e4de0bc06e407b718a648ed0ec7ace22e9937ec6
Rosevil15 <imbaintex@gmail.com>
2025-03-01 14:23:34 +0800

    Added sources on Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,34 +1,49 @@
 import { 
-    IonButton,
-    IonButtons,
-      IonContent, 
-      IonHeader, 
-      IonMenuButton, 
-      IonPage, 
-      IonTitle, 
-      IonToolbar, 
-      useIonRouter
-  } from '@ionic/react';
-  
-  const Login: React.FC = () => {
-    const navigation = useIonRouter();
-    const doLogin = () => {
-        navigation.push('/it35-lab/app','forward','replace');
-    }
-    return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonTitle>Login</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent className='ion-padding'>
+  IonButton,
+  IonButtons,
+  IonContent, 
+  IonHeader, 
+  IonMenuButton, 
+  IonPage, 
+  IonTitle, 
+  IonToolbar, 
+  IonList,
+  IonItem,
+  IonInput,
+  useIonRouter
+} from '@ionic/react';
+
+const Login: React.FC = () => {
+  const navigation = useIonRouter();
+  const doLogin = () => {
+      navigation.push('/it35-lab/app','forward','replace');
+  }
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Login</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+    
+      <IonContent className='ion-padding'>
+        <IonList>
+   
+        <IonItem>
+            <IonInput label="Email" type="email" placeholder="email@domain.com"></IonInput>
+          </IonItem>
+
+          <IonItem>
+            <IonInput label="Password" type="password" value="password"></IonInput>
+          </IonItem>
+
+        </IonList>
         <IonButton onClick={() => doLogin()} expand="full">
-            Login    
-            </IonButton>
-        </IonContent>
-      </IonPage>
-    );
-  };
-  
-  export default Login;
+          Login    
+        </IonButton>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Login;
