fd0bcbbe91a1b46bc12023740595e4b31a45c00b
Rosevil15 <imbaintex@gmail.com>
2025-03-01 15:11:14 +0800

    Updates

--- assets/swipe-back-legacy-BLZxniV5.js
+++ assets/swipe-back-legacy-BLZxniV5.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CqGEHzre.js","./index3-legacy-kspIPMgZ.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-Cr8YqJ4X.js","./index3-legacy-B7Q7Cuiu.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
