14fed1d0cec8c87dcdc13a6e154c33951a81820f
Rosevil15 <imbaintex@gmail.com>
2025-03-05 14:03:33 +0800

    Updates

--- assets/ios.transition-legacy-DsL7QC-l.js
+++ assets/ios.transition-legacy-DsL7QC-l.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-D2aq1YZo.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-DcOiVJbG.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
