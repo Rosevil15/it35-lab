5a6e5460973fbd7a9389d9efa835f00181551f36
Rosevil15 <imbaintex@gmail.com>
2025-03-18 21:14:35 +0800

    Updates

--- assets/ios.transition-legacy-2rHq1Jc2.js
+++ assets/ios.transition-legacy-2rHq1Jc2.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-MMVo2jNB.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-DeqkK1z8.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/keyboard2-legacy-DmR-5-A9.js
+++ assets/keyboard2-legacy-DmR-5-A9.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-MMVo2jNB.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-DeqkK1z8.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
