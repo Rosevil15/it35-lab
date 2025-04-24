7031c03b7e66e9ded3b2721bf66412e7e1cab227
Rosevil15 <imbaintex@gmail.com>
2025-04-24 09:05:45 +0800

    Updates

--- assets/ios.transition-legacy-DyjeBCSC.js
+++ assets/ios.transition-legacy-DyjeBCSC.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DNp9wI0m.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-Bxdqvbnu.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-UTtWeSGY.js
+++ assets/swipe-back-legacy-UTtWeSGY.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DNp9wI0m.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-Bxdqvbnu.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
